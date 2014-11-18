90c49270696f0dae064d4c7350abd4d133d1f83a
c3h3 <c3h3.tw@gmail.com>
2014-11-18 13:33:19 +0800

    [DEBUG] modify the env. variables into the absolute path

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if [ ! -f /.rabbitmq_password_set ]; then
 	/set_rabbitmq_password.sh
 fi
 
-chown -R rabbitmq:rabbitmq $RABBITMQ_LOG_BASE
-chown -R rabbitmq:rabbitmq $RABBITMQ_MNESIA
+chown -R rabbitmq:rabbitmq /data/log
+chown -R rabbitmq:rabbitmq /data/mnesia
 
 exec /usr/sbin/rabbitmq-server
