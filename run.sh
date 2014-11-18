da193cdaed19301380ccf193d4cef1cb2f88a8ee
c3h3 <c3h3.tw@gmail.com>
2014-11-18 13:03:31 +0800

    [DEBUG] modify run.sh (add $ in front of env. variables

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@ if [ ! -f /.rabbitmq_password_set ]; then
 	/set_rabbitmq_password.sh
 fi
 
-chown -R rabbitmq:rabbitmq RABBITMQ_LOG_BASE
-chown -R rabbitmq:rabbitmq RABBITMQ_MNESIA
+chown -R rabbitmq:rabbitmq $RABBITMQ_LOG_BASE
+chown -R rabbitmq:rabbitmq $RABBITMQ_MNESIA
 
 exec /usr/sbin/rabbitmq-server
